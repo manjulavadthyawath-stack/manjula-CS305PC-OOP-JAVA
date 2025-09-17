acf05e789ea16648cc158e6cbe26d4ef2befdc98
manjulavadthyawath-stack <manjulavadthyawath@gmail.com>
2025-09-17 11:23:37 +0530

    Add files via upload

---
+++ LAB3P1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class LAB3P1{
+	public static void main(String...args){
+	Scanner s = new Scanner(System.in);
+	int a = s.nextInt();
+	int i=2;
+	while(i<n){
+		int j=2;
+		while(j
+	}
+}
+}
+	
